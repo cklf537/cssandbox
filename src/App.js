12368e4b7fae4642bb1fb43064d2f32c12824c2a
cklf <cklf537@gmail.com>
2021-02-25 15:49:51 +0530

    Initial commit with material-ui configuration.

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import RWHLayout from "./components/layout";
+
+function App() {
+  return (
+    <RWHLayout />
+  );
+}
+
+export default App;
---
+++ src/components/layout.js
@@ -0,0 +1,9 @@
+const RWHLayout = ()=>{
+    return(
+        <div>
+            Welcome to Remote Work House!.
+        </div>
+    );
+}
+
+export default RWHLayout;
